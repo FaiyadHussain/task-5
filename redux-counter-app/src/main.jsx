e1b653a7b1bdc2b6318337123298f10c7c442615
FaiyadHussain <faiyadhussain2001@gmail.com>
2025-04-15 08:29:06 +0530

    Update main.jsx

--- redux-counter-app/src/main.jsx
+++ redux-counter-app/src/main.jsx
@@ -1,12 +1,12 @@
 // src/index.js or src/main.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // 👈 Correct import for React 18+
+import ReactDOM from 'react-dom/client'; 
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-// 👇 Use createRoot instead of render
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
